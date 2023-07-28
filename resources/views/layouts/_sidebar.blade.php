565ff76d43723254debf09e0a2bd4ee3035ecf60
lelutmaonk <putuadiputra51@gmail.com>
2023-07-28 22:24:16 +0800

    init

---
+++ resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,64 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+      {{-- <div class="sidebar-brand-icon">
+        <img src="img/logo/logo2.png">
+      </div> --}}
+      <div class="sidebar-brand-text mx-3">PBGIA23</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+    <li class="nav-item">
+      <a class="nav-link" href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+      Features
+    </div>
+    {{-- master --}}
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMaster" aria-expanded="true"
+        aria-controls="collapseForm">
+        <i class="fab fa-fw fa-wpforms"></i>
+        <span>Master</span>
+      </a>
+      <div id="collapseMaster" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Master</h6>
+          <a class="collapse-item" href="">Vendor Teknik</a>
+          <a class="collapse-item" href="">Vendor Tibet</a>
+          <a class="collapse-item" href="">Nomor SPBJ</a>
+          <a class="collapse-item" href="">Nomor PK</a>
+        </div>
+      </div>
+    </li>
+    {{-- end master --}}
+    {{-- main menu --}}
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMenu" aria-expanded="true"
+        aria-controls="collapseForm">
+        <i class="fab fa-fw fa-wpforms"></i>
+        <span>Menu</span>
+      </a>
+      <div id="collapseMenu" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Menu</h6>
+          <a class="collapse-item" href="">Pelayanan</a>
+          <a class="collapse-item" href="">Aruna</a>
+          <a class="collapse-item" href="">Teknik</a>
+          <a class="collapse-item" href="">Tibet</a>
+          <a class="collapse-item" href="">Tibet</a>
+          <a class="collapse-item" href="">KHS</a>
+        </div>
+      </div>
+    </li>
+    {{-- end main menu --}}
+    <li class="nav-item">
+      <a class="nav-link" href="ui-colors.html">
+        <i class="fas fa-fw fa-palette"></i>
+        <span>UI Colors</span>
+      </a>
+    </li>
+  </ul>
+  <!-- Sidebar -->
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>Title</title>
+  <link href="{{ asset('templates/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('templates/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('templates/css/ruang-admin.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    @include('layouts._sidebar')
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+       
+        @include('layouts._topbar')
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Blank Page</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="">Home</a></li>
+              <li class="breadcrumb-item">Pages</li>
+              <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+            </ol>
+          </div>
+
+          {{-- CONTENT --}}
+          @yield('content')
+          {{-- END CONTENT --}}
+
+
+        </div>
+        <!---Container Fluid-->
+      </div>
+      @include('layouts._footer')
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="{{ asset('templates/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('templates/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('templates/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('templates/js/ruang-admin.min.js') }}"></script>
+
+</body>
+
+</html>
